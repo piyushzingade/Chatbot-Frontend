0f4f11e437b0b6392537ceadcc430f0a067cdcf2
Piyushzingade <piyushzingade@gmail.com>
2024-07-23 23:46:21 +0530

    updated Options file

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,44 +1,39 @@
-import React from 'react';
-
 
 
 export default function Options() {
     return (
-
         <div className="container mx-auto max-w-[720px] px-4 md:px-0 justify-center items-center mt-12">
-            <div className='flex '>
-                <div className="flex">
-                    <img className="w-[24px] h-[24px]" src="./public/img.svg" alt="" />
-                    <p className='px-2' >Get Started</p>
+            <div className='flex  mb-6'>
+                <div className="flex items-center">
+                    <img className="w-6 h-6" src="./public/img.svg" alt="Get Started" />
+                    <p className='px-2'>Get Started</p>
                 </div>
             </div>
 
-            <div className="container mx-auto max-w-[720px] gap-x-[16px] px-4 md:px-0 flex justify-center items-center mt-6">
-                <div className="w-[168px]  h-[100px] p-4 rounded-lg bg-white">
-                    <div className='w-[136px] h-[68px]'>
-                        <img className="w-[24px] h-[24px]" src="./public/image1.png" alt="image1" />
-                        <p className='font-semibold text-xs mt-3'>Course Resgrisation Help</p>
-                    </div>
-                </div>
-                <div className=" w-[168px]  h-[100px] p-4 rounded-lg bg-white">
-                    <div className="w-[136px] h-[68px]">
-                        <img className="w-[24px] h-[24px]" src="./public/image2.png" alt="image2" />
-                        <p className='font-semibold text-xs mt-3' >Internship Opportunities</p>
-                    </div>
-                </div>
-                <div className=" w-[168px]  h-[100px] p-4 rounded-lg bg-white">
-                    <div className="w-[136px] h-[68px]">
-                        <img className="w-[24px] h-[24px]" src="./public/image3.png" alt="image3" />
-                        <p className='font-semibold text-xs mt-3'>Library Resources</p>
-                    </div>
-                </div>
-                <div className=" w-[168px]  h-[100px] p-4 rounded-lg bg-white">
-                    <div className="w-[136px] h-[68px]">
-                        <img className="w-[24px] h-[24px]" src="./public/image4.png" alt="image4" />
-                        <p className='font-semibold text-xs mt-3'>Graduation Requirement </p>
-                    </div>
-                </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+                <OptionsButton image="./public/image1.png" title="Course Registration Help" />
+                <OptionsButton image="./public/image2.png" title="Internship Opportunities" />
+                <OptionsButton image="./public/image3.png" title="Library Resources" />
+                <OptionsButton image="./public/image4.png" title="Graduation Requirements" />
             </div>
         </div>
     );
-};
+}
+
+export interface OptionsProps {
+    image: string;
+    title: string;
+}
+
+export function OptionsButton(props: OptionsProps) {
+    const { image, title } = props;
+    return (
+        <button className="p-4 text-left rounded-lg bg-white mt-5 hover:shadow-xl focus:shadow-xl duration-150">
+            <div className=''>
+                <img className="h-6 w-6 "
+                    src={image} alt="image1" />
+                <p className='font-semibold text-xs mt-3'>{title}</p>
+            </div>
+        </button>
+    )
+}
